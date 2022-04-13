d727df038305c21beb41867897344db41179e59c
Hassan Chowdhry <mohammedchowdhry11@gmail.com>
2022-04-13 23:10:52 +0500

    checkpoint

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,18 @@
-import UserProfile from './components/UserProfile';
-import Photos from './components/Photos';
-import ImageArray from './components/Images';
-import './App.css';
+import UserProfile from "./components/UserProfile";
+import Photos from "./components/Photos";
+import ImageArray from "./components/Images";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+
       <UserProfile />
-      {ImageArray.map((image) => <Photos source={image} />)}
       
-    
+      {ImageArray.map((image, index) => (
+        <Photos source={image} alt={`photo-${index + 1}`} key={Math.random()} />
+      ))}
+
     </div>
   );
 }
--- my-app/src/components/Photos.js
+++ my-app/src/components/Photos.js
@@ -2,10 +2,13 @@ import './Photos.css';
 
 
 function Photos(props) {
+  const onClickHandler = () => {
+    alert(props.alt)
+  }
 
   return (
     <figure className = "frame">
-      <img src={props.source} alt="" className="image"/>
+      <img src={props.source} alt={props.alt} className="image" onClick = {onClickHandler} />
     </figure>
   )
 }
