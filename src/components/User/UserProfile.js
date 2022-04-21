25fd0974bdae7297b98811cd499d857cd4abf71e
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2022-04-21 06:43:23 +0500

    updated with (click to close) and (pfp open portfolio) (#9)

--- src/components/User/UserProfile.js
+++ src/components/User/UserProfile.js
@@ -3,20 +3,22 @@ import photo from "../../Images/Hassan-Picture.jpg";
 
 function UserProfile() {
   const onClickHandler = () => {
-    alert("add modal");
+    window.open("https://hassanchowdhry.github.io/Portfolio/", "_blank")
   };
 
   return (
     <div className="profile">
-      
       <div className="image-container">
         <img src={photo} alt="" onClick={onClickHandler} />
       </div>
-      
-        <strong> Hassan Chowdhry</strong>
 
-        <span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam obcaecati modi exercitationem a cumque consequuntur fugit eius quaerat? Laborum, odit. </span>
+      <strong> Hassan Chowdhry</strong>
 
+      <span>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam obcaecati
+        modi exercitationem a cumque consequuntur fugit eius quaerat? Laborum,
+        odit.
+      </span>
     </div>
   );
 }
